dd3bac6fad2d7402ed0b861e582333c4ea93ab2f
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2021-10-20 14:22:07 +0200

    feat: :sparkles: routes & basic mongodb connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type application struct {
@@ -12,33 +21,56 @@ type application struct {
 	infoLog  *log.Logger
 }
 
+func init() {
+	err := godotenv.Load("../../.env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
+	port := os.Getenv("PORT")
+	addr := flag.String("addr", port, "HTTP network address")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Connect to DB
+	uri := os.Getenv("MONGODB_URI")
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	fmt.Println(databases)
 
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snip", app.showSnip)
-	mux.HandleFunc("/snip/create", app.createSnip)
-
-	fileServer := http.FileServer(http.Dir("../../ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
-	errorLog.Fatal(err)
+	errSrv := srv.ListenAndServe()
+	errorLog.Fatal(errSrv)
 }
